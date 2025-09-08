3785e9e0b0c4662ca058f9433c98c72017eb45d7
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-09-07 23:57:48 -0300

    refactor: move popular movies request to api folder

---
+++ src/api/getPopularMovies.ts
@@ -0,0 +1,21 @@
+export type Movie = {
+  id: number
+  title: string
+  director?: string
+  genre_ids: number[]
+  genres?: string[]
+  poster_path: string
+}
+
+export async function getPopularMovies(): Promise<Movie[]> {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=pt-BR&page=1`
+  )
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch popular movies')
+  }
+
+  const data = await response.json()
+  return data.results
+}
--- src/hooks/useRawMovies.ts
+++ src/hooks/useRawMovies.ts
@@ -1,25 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
-
-export type Movie = {
-  id: number
-  title: string
-  director?: string
-  genre_ids: number[]
-  genres?: string[]
-  poster_path: string
-}
+import { getPopularMovies, type Movie } from '../api/getPopularMovies'
 
 export function useRawMovies() {
   const { data: rawMovies, isError } = useQuery<Movie[]>({
     queryKey: ['movies'],
-    queryFn: async () => {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=pt-BR&page=1`
-      )
-
-      const data = await response.json()
-      return data.results
-    },
+    queryFn: getPopularMovies,
   })
 
   return { rawMovies, isError }
