7cf67b41c6227260167c803028d358b84d622915
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-09-23 17:24:07 -0300

    feat: add theme provider for theme switching

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './app.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { App } from './app'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>
 )
