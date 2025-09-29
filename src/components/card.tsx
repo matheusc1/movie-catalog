c2b82c8dead2440fdb3d8254054f0188bd9b0ddd
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-09-29 12:44:03 -0300

    fix: adjust image size to ensure even dimensions

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,7 @@ export function Card({ movie }: MovieProps) {
     <div className="w-[280px] h-fit flex flex-col items-center border-1 dark:bg-neutral-800 border-neutral-175 dark:border-neutral-750 rounded-[10px] shadow-sm shadow-neutral-100/70 dark:shadow-neutral-950/70">
       <img
         src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-        className="w-[107px] h-[145px] content-center"
+        className="w-[108px] h-[145px] content-center"
         alt={`${movie.title} banner`}
       />
       <div className="h-0.25 w-full bg-neutral-150 dark:bg-neutral-725" />
--- src/components/cardSkeleton.tsx
+++ src/components/cardSkeleton.tsx
@@ -1,7 +1,7 @@
 export function CardSkeleton() {
   return (
     <div className="w-[280px] h-fit flex flex-col items-center border-1 dark:bg-neutral-800 border-neutral-175 dark:border-neutral-750 rounded-[10px]">
-      <div className="w-[107px] h-[145px] bg-neutral-200 dark:bg-neutral-700 animate-pulse" />
+      <div className="w-[108px] h-[145px] bg-neutral-200 dark:bg-neutral-700 animate-pulse" />
       <div className="h-0.25 w-full bg-neutral-150 dark:bg-neutral-725" />
       <div className="w-full p-4 space-y-5">
         <div className="h-6 w-48 bg-neutral-200 dark:bg-neutral-700 rounded-md animate-pulse" />
